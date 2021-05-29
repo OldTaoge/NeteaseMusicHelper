0837697dd6612dfb7b9634614b9dc5332ce83a2c
OldTaoge <admin@oldtaoge.space>
2021-05-29 21:54:11 +0800

    防重下载

--- NEM_Action.py
+++ NEM_Action.py
@@ -10,6 +10,9 @@ def Action_radar_to_pl():
     radar_pl = NEM_Playlist.Playlist_getPlaylistDetail(3136952023)
     tracks_list = radar_pl["playlist"]["tracks"]
     track_id_str = _Action_TrackListToStr(tracks_list)
+    if track_id_str in global_data["data"]["radarIdHash"]:
+        return -1
+    global_data["data"]["radarIdHash"].append(track_id_str)
     npl = NEM_Playlist.Playlist_getPlaylistCreate(time.strftime(global_data["config"]["dailyNameTemplate"]["radar"],
                                                                 time.localtime()))
     NEM_Playlist.Playlist_getPlaylistAdd(npl["playlist"]["id"], track_id_str)
